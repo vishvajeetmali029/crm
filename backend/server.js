ba380839cd32b7ac8c079f010c3407343a0cf28d
vishvajeetmali029 <vishvajeetmali029@gmail.com>
2026-04-29 00:58:48 +0530

    first commit

--- backend/server.js
+++ backend/server.js
@@ -9,7 +9,14 @@ dotenv.config();
 const app = express();
 const port = process.env.PORT || 5000;
 
-app.use(cors());
+app.use(
+  cors({
+    origin: ["https://crm-ak7v.vercel.app", "http://localhost:3000"],
+    methods: ["GET", "POST", "PUT", "DELETE"],
+    credentials: true,
+  })
+);)
+);
 app.use(express.json());
 
 // Routes
